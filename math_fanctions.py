5f1429f76522a770d2bb3d690fe41a9cf4e932f4
refael_saidov <refaelsaidov1@gmail.com>
2025-06-19 15:04:49 +0300

    הוסף פונקציית פקטוריאל

--- math_fanctions.py
+++ math_fanctions.py
@@ -3,7 +3,15 @@
 def welcome():
     """פונקציה לברכה"""
     print("ברוכים הבאים לספרייה של פונקציות אלגוריתמיות!")
-
+def factorial(n):
+    """פונקציה רקורסיבית לחישוב פקטוריאל"""
+    if n < 0:
+        return "אין פקטוריאל למספרים שליליים"
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
 if __name__ == "__main__":
     welcome()
-
+number = int(input("הכנס מספר שלם כדי לחשב את הפקטוריאל שלו: "))
+result = factorial(number)
+print(f"הפקטוריאל של {number} הוא: {result}")
