e966d14abdb77104a6fb59e89e67282f42d8b594
Daniel_Saidov <ddny3622@gmail.com>
2025-06-19 15:12:37 +0300

    הוסף פונקציית בדיקה למספר ראשוני

--- math_fanctions.py
+++ math_fanctions.py
@@ -4,6 +4,17 @@ def welcome():
     """פונקציה לברכה"""
     print("ברוכים הבאים לספרייה של פונקציות אלגוריתמיות!")
 
+
+def is_prime(n, i=2):
+    """בודקת רקורסיבית אם מספר הוא ראשוני"""
+    if n <= 1:
+        return False
+    if i * i > n:
+        return True
+    if n % i == 0:
+        return False
+    return is_prime (n, i + 1)
+
 if __name__ == "__main__":
     welcome()
-
+    print(is_prime(27))
